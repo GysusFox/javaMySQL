0c87e4f59d081554fcb1567712bcd3c827b5d87b
Jesush_18 <therdtree966@gmail.com>
2022-07-01 21:07:16 -0300

    add derictivas de mongo

--- javaMongo/src/main/java/src/Main.java
+++ javaMongo/src/main/java/src/Main.java
@@ -2,6 +2,7 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        
+        CConexion objectConexion = new CConexion();
+        objectConexion.crearConexion();
     }
 }
