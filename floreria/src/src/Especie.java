76fa019cb7405689a14fbe53bf7eeba30b0b4bab
carlosmassaa <carlosmb55555@gmail.com>
2022-07-09 17:20:07 -0300

    JESUUUUU

--- floreria/src/src/Especie.java
+++ floreria/src/src/Especie.java
@@ -1,14 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package src;
 
-/**
- *
- * @author Admin
- */
+
 public class Especie {
+    
+    
+    String nombre;
+    String epocaFloracion;
+    String estacioPlantacion;
+    String tipoSueloPlantacion;
+    int tiempoExposicion;
+
+    public Especie(String nombre, String epocaFloracion, String estacioPlantacion, String tipoSueloPlantacion, int tiempoExposicion) {
+        this.nombre = nombre;
+        this.epocaFloracion = epocaFloracion;
+        this.estacioPlantacion = estacioPlantacion;
+        this.tipoSueloPlantacion = tipoSueloPlantacion;
+        this.tiempoExposicion = tiempoExposicion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEpocaFloracion() {
+        return epocaFloracion;
+    }
+
+    public void setEpocaFloracion(String epocaFloracion) {
+        this.epocaFloracion = epocaFloracion;
+    }
+
+    public String getEstacioPlantacion() {
+        return estacioPlantacion;
+    }
+
+    public void setEstacioPlantacion(String estacioPlantacion) {
+        this.estacioPlantacion = estacioPlantacion;
+    }
+
+    public String getTipoSueloPlantacion() {
+        return tipoSueloPlantacion;
+    }
+
+    public void setTipoSueloPlantacion(String tipoSueloPlantacion) {
+        this.tipoSueloPlantacion = tipoSueloPlantacion;
+    }
+
+    public int getTiempoExposicion() {
+        return tiempoExposicion;
+    }
+
+    public void setTiempoExposicion(int tiempoExposicion) {
+        this.tiempoExposicion = tiempoExposicion;
+    }
+    
+    
+    
     
 }
--- floreria/src/src/Pedidos.java
+++ floreria/src/src/Pedidos.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package src;
 
-/**
- *
- * @author Admin
- */
+
 public class Pedidos {
+    
+    
+    String fechaVenta; // DIA/MES/AÑO
+    int importeTotal;
+    String [ ] listaFloresSolicitadas;
+    int cantidad;
+
+    public Pedidos(String fechaVenta, int importeTotal, String[] listaFloresSolicitadas, int cantidad) {
+        this.fechaVenta = fechaVenta;
+        this.importeTotal = importeTotal;
+        this.listaFloresSolicitadas = listaFloresSolicitadas;
+        this.cantidad = cantidad;
+    }
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public int getImporteTotal() {
+        return importeTotal;
+    }
+
+    public void setImporteTotal(int importeTotal) {
+        this.importeTotal = importeTotal;
+    }
+
+    public String[] getListaFloresSolicitadas() {
+        return listaFloresSolicitadas;
+    }
+
+    public void setListaFloresSolicitadas(String[] listaFloresSolicitadas) {
+        this.listaFloresSolicitadas = listaFloresSolicitadas;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+    
     
 }
