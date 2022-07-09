9e627fd34284922cd7df6fa5e6be079312c20493
TheYisus <alfonzo19hernandez@gmail.com>
2022-07-09 16:17:51 -0300

    changes

---
+++ floreria/src/app/Main.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JFrame;
+import src.Conexion;
+
+public class Main {
+    static Conexion con;
+    static Statement stmt = null;
+    
+    public static void main(String[] args) throws SQLException {
+        con = new Conexion();
+        Connection reg = con.getConnection();
+        
+        JFrame inicio = new Inicio();
+        inicio.setVisible(true);
+        
+        //stmt = reg.createStatement();
+        //stmt.executeUpdate("INSERT INTO `usuarios`(`nombre`) VALUES('Jesus')");
+    }
+}
---
+++ floreria/src/src/Conexion.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    
+    private static Connection conn;
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String user = "root";
+    private static final String password = "";
+    private static final String url = "jdbc:mysql://localhost:3306/floreria_store";
+    
+    public Conexion(){
+        conn = null;
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url,user,password);
+            if(conn != null){
+                System.out.println("Conexion establecida!!");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error al conectar!! | " + e);
+        }
+    }
+    
+    // tomamos la conexion de la base de datos 
+    public Connection getConnection(){
+        return conn;
+    }
+    
+    // CERRAR LA CONEXION A LA BASE DE DATOS
+    public void desconectar(){
+        conn = null;
+        if(conn == null){
+            System.out.println("Conexion cerrada!!");
+        }
+    }
+}
